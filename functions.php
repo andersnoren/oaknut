c1c5bdfa49fe5998361600dee6bfbd38ac597585
Anders <anders@andersnoren.se>
2022-10-12 11:59:25 +0200

    So many button styles

--- functions.php
+++ functions.php
@@ -35,9 +35,52 @@ function acorn_register_block_styles() {
 
 	// Button: Shadow
 	register_block_style( 'core/button', array(
-		'name'  	=> 'acorn-shadow',
-		'label' 	=> esc_html__( 'Drop Shadow', 'poe' ),
+		'name'  	=> 'acorn-button-shadow',
+		'label' 	=> esc_html__( 'Drop Shadow', 'acorn' ),
 	) );
+
+	// Button: Shape: Angle
+	register_block_style( 'core/button', array(
+		'name'  	=> 'acorn-button-shape-angle',
+		'label' 	=> esc_html__( 'Shape: Angle', 'acorn' ),
+	) );
+
+	// Button: Shape: Bevel
+	register_block_style( 'core/button', array(
+		'name'  	=> 'acorn-button-shape-bevel',
+		'label' 	=> esc_html__( 'Shape: Bevel', 'acorn' ),
+	) );
+
+	// Button: Shape: Chevron Left
+	register_block_style( 'core/button', array(
+		'name'  	=> 'acorn-button-shape-chevron-left',
+		'label' 	=> esc_html__( 'Shape: Chevron Left', 'acorn' ),
+	) );
+
+	// Button: Shape: Chevron Right
+	register_block_style( 'core/button', array(
+		'name'  	=> 'acorn-button-shape-chevron-right',
+		'label' 	=> esc_html__( 'Shape: Chevron Right', 'acorn' ),
+	) );
+
+	// Button: Shape: Pointed
+	register_block_style( 'core/button', array(
+		'name'  	=> 'acorn-button-shape-pointed',
+		'label' 	=> esc_html__( 'Shape: Pointed', 'acorn' ),
+	) );
+
+	// Button: Shape: Rabbet
+	register_block_style( 'core/button', array(
+		'name'  	=> 'acorn-button-shape-rabbet',
+		'label' 	=> esc_html__( 'Shape: Rabbet', 'acorn' ),
+	) );
+
+	// Button: Shape: Ribbon
+	register_block_style( 'core/button', array(
+		'name'  	=> 'acorn-button-shape-ribbon',
+		'label' 	=> esc_html__( 'Shape: Ribbon', 'acorn' ),
+	) );
+
 	
 }
 add_action( 'init', 'acorn_register_block_styles' );
